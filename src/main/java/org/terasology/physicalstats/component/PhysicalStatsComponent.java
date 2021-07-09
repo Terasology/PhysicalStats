7a2c98d9400b976dd240d51547f4dbef15663d14
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:16 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/physicalstats/component/PhysicalStatsComponent.java
+++ src/main/java/org/terasology/physicalstats/component/PhysicalStatsComponent.java
@@ -1,28 +1,15 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.physicalstats.component;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * This component is used for storing information about an entity's base physical stats or attributes.
  * It's up to other modules to define how exactly each stat is used - the descriptions given here are guidelines.
  */
-public class PhysicalStatsComponent implements Component {
+public class PhysicalStatsComponent implements Component<PhysicalStatsComponent> {
     /** The strength stat affects how much physical damage an entity does upon striking a target. */
     @Replicate
     public int strength;
--- src/main/java/org/terasology/physicalstats/component/PhysicalStatsModifiersListComponent.java
+++ src/main/java/org/terasology/physicalstats/component/PhysicalStatsModifiersListComponent.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.physicalstats.component;
 
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +14,7 @@ import java.util.Map;
  *
  * Note: Make sure that the entity you are attaching this to has a PhysicalStatsComponent.
  */
-public class PhysicalStatsModifiersListComponent implements Component {
+public class PhysicalStatsModifiersListComponent implements Component<PhysicalStatsModifiersListComponent> {
     /** A map of physical stats modifiers being applied to an entity. */
     public Map<String, PhysicalStatsModifierComponent> modifiers = new HashMap<String, PhysicalStatsModifierComponent>();
 }
