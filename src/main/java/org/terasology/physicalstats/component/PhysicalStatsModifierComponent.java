9cd3f4c758d17b924170efd8eaaf78a8c27e98ee
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 22:15:37 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#10)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Michael Pollind <mpollind@gmail.com>
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/physicalstats/component/PhysicalStatsModifierComponent.java
+++ src/main/java/org/terasology/physicalstats/component/PhysicalStatsModifierComponent.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.physicalstats.component;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.network.Replicate;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.reflection.MappedContainer;
 
 /**
@@ -28,7 +15,7 @@ import org.terasology.reflection.MappedContainer;
  * Note: Only one of this kind of modifier should be applied to either items or buffs at a time.
  */
 @MappedContainer
-public class PhysicalStatsModifierComponent implements Component {
+public class PhysicalStatsModifierComponent implements Component<PhysicalStatsModifierComponent> {
     /**
      * This stores this particular modifier's ID. Each modifier is normally intended to have a different ID, barring
      * the scenario where certain effects can replace older ones.
@@ -63,4 +50,16 @@ public class PhysicalStatsModifierComponent implements Component {
     /** The luck stat will provide benefits to many different actions in the future. */
     @Replicate
     public int luck;
+
+    @Override
+    public void copyFrom(PhysicalStatsModifierComponent other) {
+        this.id = other.id;
+        this.strength = other.strength;
+        this.dexterity = other.dexterity;
+        this.constitution = other.constitution;
+        this.agility = other.agility;
+        this.endurance = other.endurance;
+        this.charisma = other.charisma;
+        this.luck = other.luck;
+    }
 }
--- src/main/java/org/terasology/physicalstats/component/PhysicalStatsModifiersListComponent.java
+++ src/main/java/org/terasology/physicalstats/component/PhysicalStatsModifiersListComponent.java
@@ -1,21 +1,9 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.physicalstats.component;
 
-import org.terasology.engine.entitySystem.Component;
+import com.google.common.collect.Maps;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +15,14 @@ import java.util.Map;
  *
  * Note: Make sure that the entity you are attaching this to has a PhysicalStatsComponent.
  */
-public class PhysicalStatsModifiersListComponent implements Component {
-    /** A map of physical stats modifiers being applied to an entity. */
+public class PhysicalStatsModifiersListComponent implements Component<PhysicalStatsModifiersListComponent> {
+    /**
+     * A map of physical stats modifiers being applied to an entity.
+     */
     public Map<String, PhysicalStatsModifierComponent> modifiers = new HashMap<String, PhysicalStatsModifierComponent>();
+
+    @Override
+    public void copyFrom(PhysicalStatsModifiersListComponent other) {
+        other.modifiers = Maps.newHashMap(other.modifiers);
+    }
 }
