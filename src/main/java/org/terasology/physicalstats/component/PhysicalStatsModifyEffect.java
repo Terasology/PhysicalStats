de087a2d2c97e5ea9643123049bec33f0964901c
Nail Khanipov <foxflameless@gmail.com>
2021-12-09 01:28:40 +0300

    feat(gestaltv7-eventsystem): Migration Event and @ReceiveEvent to gestalt's (#11)
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/physicalstats/component/PhysicalStatsModifyEffect.java
+++ src/main/java/org/terasology/physicalstats/component/PhysicalStatsModifyEffect.java
@@ -1,22 +1,8 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.physicalstats.component;
 
 import org.terasology.engine.context.Context;
-import org.terasology.engine.core.Time;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.logic.delay.DelayManager;
 import org.terasology.physicalstats.event.OnPhysicalStatsModifierAddedEvent;
@@ -26,27 +12,26 @@ import org.terasology.physicalstats.event.OnPhysicalStatsModifierRemovedEvent;
  * Class that handles the application and removal of permanent or temporary physical stats modifiers to/from an entity.
  */
 public class PhysicalStatsModifyEffect implements IPhysicalStatsModifyEffect {
-    /**  Used for getting the current time. */
-    private final Time time;
 
-    /** Reference to the delay manager for adding temporary modifiers. */
+    /**
+     * Reference to the delay manager for adding temporary modifiers.
+     */
     private final DelayManager delayManager;
 
     /**
      * Create an instance of this class using the passed in Context to get the Time and DelayManager instances.
      *
-     * @param context   Reference to the current context that this object is running in.
+     * @param context Reference to the current context that this object is running in.
      */
     public PhysicalStatsModifyEffect(Context context) {
-        this.time = context.get(Time.class);
         this.delayManager = context.get(DelayManager.class);
     }
 
     @Override
     public void applyMod(EntityRef instigator, EntityRef entity, PhysicalStatsModifierComponent modifier, long duration) {
         // If the duration is less than or equal to 0, throw an exception as this is an invalid value for duration.
-        if (duration <= 0) {
-            throw(new IllegalArgumentException("The inputted duration of " + duration + " falls outside the permitted boundaries."));
+        if (duration < 0) {
+            throw (new IllegalArgumentException("The inputted duration of " + duration + " falls outside the permitted boundaries."));
         }
 
         // Get the list of physical stats modifiers on this entity.
@@ -67,10 +52,12 @@ public class PhysicalStatsModifyEffect implements IPhysicalStatsModifyEffect {
             modifiersList.modifiers.replace(modifier.id, modifier);
         }
 
-        // If the duration is greater than or equal to 0, add this to the DelayManager. This indicates that the effect
+        //TODO: figure out whether it is correct to treat a duration of 0 as permanent.
+
+        // If the duration is greater than 0, add this to the DelayManager. This indicates that the effect
         // is temporary. Otherwise, the effect will be treated as permanent unless another entity manually removes the
         // effect.
-        if (duration >= 0) {
+        if (duration > 0) {
             delayManager.addDelayedAction(entity, "" + modifier.id, duration);
         }
 
@@ -111,12 +98,11 @@ public class PhysicalStatsModifyEffect implements IPhysicalStatsModifyEffect {
     }
 
     /**
-     * Remove a PhysicalStatsModifier from the entity. If the modifier doesn't exist (can't find the ID), nothing will
-     * be removed.
+     * Remove a PhysicalStatsModifier from the entity. If the modifier doesn't exist (can't find the ID), nothing will be removed.
      *
-     * @param instigator    Entity that is removing this modifier.
-     * @param entity        Entity that the modifier is being removed from.
-     * @param id            ID of the entity to be removed.
+     * @param instigator Entity that is removing this modifier.
+     * @param entity Entity that the modifier is being removed from.
+     * @param id ID of the entity to be removed.
      */
     public static void removeMod(EntityRef instigator, EntityRef entity, String id) {
         // Get the list of physical stats modifiers on this entity.
